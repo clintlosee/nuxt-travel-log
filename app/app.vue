7c82c82c0769fd050bfabc5b71555611c5f716aa
Clint Losee <clint.losee@gmail.com>
2025-05-03 08:43:39 -0600

    :sparkles: setup tailwind, daisyui and icons

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-primary">
+      Secondary
+      <Icon name="tabler:map" />
+    </button>
+  </div>
+</template>
