c1005a4948e9ec159b98486bcfa62ba3b9afa467
Clint Losee <clint.losee@gmail.com>
2025-05-03 08:45:06 -0600

    :sparkles: setup tailwind, daisyui and icons (#49)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  future: {
+    compatibilityVersion: 4,
+  },
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   eslint: {
     config: {
       standalone: false,
