7b6764525d8056d75292a0ca332a690640428886
Clint Losee <clint.losee@gmail.com>
2025-05-04 09:06:37 -0600

    Daisy UI and setup landing page (#50)
    * :sparkles: setup tailwind, daisyui and icons
    
    * :sparkles: setup landing page

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,8 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-primary">
-      Secondary
-      <Icon name="tabler:map" />
-    </button>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
