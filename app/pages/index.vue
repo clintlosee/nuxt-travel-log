f664de0c9ddfcdbfb4a63f8982c76f86e79efc35
Clint Losee <clint.losee@gmail.com>
2025-05-04 09:00:57 -0600

    :sparkles: setup landing page

---
+++ app/pages/index.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventures with this simple travel log app. Add locations, photos, and notes to create a digital journal of your journeys.
+        </p>
+        <NuxtLink
+          to="/dashboard"
+          class="btn btn-accent"
+        >
+          Sign In With Github
+          <Icon name="tabler:brand-github" size="24" />
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
