a9d41dcc4ce96ca151fbb911c04e89e4daf306cc
Clint Losee <clint.losee@gmail.com>
2025-05-08 21:58:35 -0600

    :sparkles: Fix db type for better auth. Setup up github login. Error page. Pinia auth store.better auth setup

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,13 +12,7 @@
         <p class="py-6">
           Keep track of your travels and adventures with this simple travel log app. Add locations, photos, and notes to create a digital journal of your journeys.
         </p>
-        <NuxtLink
-          to="/dashboard"
-          class="btn btn-accent"
-        >
-          Sign In With Github
-          <Icon name="tabler:brand-github" size="24" />
-        </NuxtLink>
+        <AuthButton />
       </div>
     </div>
   </div>
