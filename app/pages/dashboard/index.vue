a831354e643f01dada25661ae5dce01f6573d8d1
Clint Losee <clint.losee@gmail.com>
2025-05-11 11:14:56 -0600

    :sparkles: sidebar naviation setup

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,3 +1,60 @@
+<script lang="ts" setup>
+const isSidebarOpen = ref(true);
+
+onMounted(() => {
+  isSidebarOpen.value = localStorage.getItem('isSidebarOpen') === 'true';
+});
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem('isSidebarOpen', isSidebarOpen.value.toString());
+}
+</script>
+
 <template>
-  <h1>You are logged in!</h1>
+  <div class="flex-1 flex">
+    <div class="bg-base-100 transition-all duration-300" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
+      <div
+        class="flex cursor-pointer hover:bg-base-200 p-2"
+        :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }"
+        @click="toggleSidebar"
+      >
+        <Icon
+          v-if="isSidebarOpen"
+          name="tabler:chevron-left"
+          size="32"
+        />
+        <Icon
+          v-else
+          name="tabler:chevron-right"
+          size="32"
+        />
+      </div>
+      <div class="flex flex-col">
+        <SidebarButton
+          label="Locations"
+          icon="tabler:map"
+          href="/dashboard"
+          :show-label="isSidebarOpen"
+        />
+        <SidebarButton
+          label="Add Location"
+          icon="tabler:circle-plus-filled"
+          href="/dashboard/add"
+          :show-label="isSidebarOpen"
+        />
+        <div class="divider" />
+        <SidebarButton
+          label="Sign Out"
+          href="/sign-out"
+          icon="tabler:logout-2"
+          :show-label="isSidebarOpen"
+        />
+      </div>
+    </div>
+
+    <div class="flex-1">
+      You are logged in
+    </div>
+  </div>
 </template>
